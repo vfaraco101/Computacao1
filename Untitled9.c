9ef147b3be7e106efba2be8b6195debdd4ad9524
vfaraco101 <vgfaraco@gmail.com>
2025-04-03 15:15:59 -0300

    Add files via upload

---
+++ Untitled9.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+
+    float peso, alt, taxa;
+    int idade,codigo;
+
+
+    printf("Codigo: %d \n");
+    printf("Idad:%d \nPeso:%f \nAltua:%f\n", idade, peso, alt);
+    printf("Taxa: %f", taxa);
+
+    scanf("%d", &codigo);
+    scanf("%d %f %f",&idade, &peso, &alt);
+    scanf("%f", &taxa);
+    return 0;
+}
