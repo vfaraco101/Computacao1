8d63bf6d6be86ee027fd776bad99fbe513661df6
vfaraco101 <vgfaraco@gmail.com>
2025-04-03 15:16:47 -0300

    Update Untitled9.c

--- Untitled9.c
+++ Untitled9.c
@@ -6,7 +6,7 @@ int main(){
 
 
     printf("Codigo: %d \n");
-    printf("Idad:%d \nPeso:%f \nAltura:%f\n", idade, peso, alt);
+    printf("Idade:%d \nPeso:%f \nAltura:%f\n", idade, peso, alt);
     printf("Taxa: %f", taxa);
 
     scanf("%d", &codigo);
