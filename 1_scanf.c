1607405a22e029607208032853149938bb2a9d86
vfaraco101 <vgfaraco@gmail.com>
2025-04-03 15:28:55 -0300

    Update 1_scanf.c

--- 1_scanf.c
+++ 1_scanf.c
@@ -4,13 +4,18 @@ int main(){
     float peso, alt, taxa;
     int idade,codigo;
 
+    printf("Dgite o codigo:\n");
+    scanf("%d", &codigo);
+
+    printf("Digite a idade, o peso e a altura:\n");
+    scanf("%d %f %f",&idade, &peso, &alt);
+
+    printf("Digite a taxa:\n");
+    scanf("%f", &taxa);
 
     printf("Codigo: %d \n");
     printf("Idade:%d \nPeso:%f \nAltura:%f\n", idade, peso, alt);
     printf("Taxa: %f", taxa);
 
-    scanf("%d", &codigo);
-    scanf("%d %f %f",&idade, &peso, &alt);
-    scanf("%f", &taxa);
     return 0;
 }
