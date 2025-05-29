78b1d3d95f1fb6cafb7fa5012416764492ddb880
vfaraco101 <vgfaraco@gmail.com>
2025-05-29 17:24:03 -0300

    Update matriz1.c

--- matriz1.c
+++ matriz1.c
@@ -4,37 +4,41 @@
 
 #define N 5
 
-void Matriz(int mat[N][N]) {
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
+void Matriz(int mat[N][N])
+{
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
             printf("%2d ", mat[i][j]);
         }
         printf("\n");
     }
 }
-
-void transposta(int mat[N][N], int matT[N][N]) {
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
+void transposta(int mat[N][N], int matT[N][N])
+{
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
             matT[i][j] = mat[j][i];
         }
     }
 }
-
-int main() {
+int main()
+{
     int mat[N][N], matT[N][N];
     srand(time(NULL));
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
             mat[i][j] = rand() % 50;
         }
     }
-
     printf("Matriz original:\n");
     Matriz(mat);
-
     transposta(mat, matT);
-
     printf("\nMatriz transposta:\n");
     Matriz(matT);
 
