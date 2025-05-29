03851df3fb31f6bde6566cdd132480bf1a1a716f
vfaraco101 <vgfaraco@gmail.com>
2025-05-29 15:22:44 -0300

    Update VetoreseFunções 4.c

--- VetoreseFunções 4.c
+++ VetoreseFunções 4.c
@@ -2,29 +2,25 @@
 #include <stdlib.h>
 #include <time.h>
 
-void preencherVetor(int v[], int n, int lim)
-{
-    srand(time(NULL));
+void preencheVetor(int v[], int n, int lim){
+srand(time(NULL));
 
-    for (int i = 0; i < n; i++)
-    {
+    for (int i = 0; i < n; i++){
         v[i] = rand() %(lim + 1);
     }
 }
 
-int main()
-{
+int main() {
     int vetor[10];
+    int tamanho = 10;
     int limite = 50;
 
-    preencherVetor(vetor, 10, limite);
+    preencheVetor(vetor, tamanho, limite);
 
     printf("Vetor preenchido: ");
-    for (int i = 0; i <10; i++)
-    {
+    for (int i = 0; i <tamanho; i++){
         printf("%d ", vetor[i]);
     }
-    printf("\n");
 
     return 0;
 }
