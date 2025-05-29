12e70753fcd7e64734e49b1b51e534f87e89ede4
vfaraco101 <vgfaraco@gmail.com>
2025-05-29 17:16:02 -0300

    Create matriz1.c

---
+++ matriz1.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define N 5
+
+void imprimirMatriz(int mat[N][N]) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            printf("%2d ", mat[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void transposta(int mat[N][N], int matT[N][N]) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            matT[i][j] = mat[j][i];
+        }
+    }
+}
+
+int main() {
+    int mat[N][N], matT[N][N];
+    srand(time(NULL));
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            mat[i][j] = rand() % 50;
+        }
+    }
+
+    printf("Matriz original:\n");
+    imprimirMatriz(mat);
+
+    transposta(mat, matT);
+
+    printf("\nMatriz transposta:\n");
+    imprimirMatriz(matT);
+
+    return 0;
+}
