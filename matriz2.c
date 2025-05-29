8c5e12c03342c54acf2a5022038f155101d74c68
vfaraco101 <vgfaraco@gmail.com>
2025-05-29 17:25:14 -0300

    Update matriz2.c

--- matriz2.c
+++ matriz2.c
@@ -1,26 +1,34 @@
 #include <stdio.h>
 #define MAX_N 10
 
-int fatorial(int n) {
+int fatorial(int n)
+{
     int f = 1;
-    for (int i = 2; i <= n; i++) {
+    for (int i = 2; i <= n; i++)
+    {
         f *= i;
     }
     return f;
 }
-int binomial(int n, int r) {
+int binomial(int n, int r)
+{
     if (r > n) return 0;
     return fatorial(n) / (fatorial(r) * fatorial(n - r));
 }
-void Pascal(int n) {
+void Pascal(int n)
+{
     int mat[MAX_N][MAX_N] = {0};
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j <= i; j++) {
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
             mat[i][j] = binomial(i, j);
         }
     }
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
             if (j <= i)
                 printf("%4d ", mat[i][j]);
             else
@@ -29,11 +37,13 @@ void Pascal(int n) {
         printf("\n");
     }
 }
-int main() {
+int main()
+{
     int n;
     printf("Digite o numero de linhas do triangulo de Pascal (1 a %d): ", MAX_N);
     scanf("%d", &n);
-    if (n < 1 || n > MAX_N) {
+    if (n < 1 || n > MAX_N)
+    {
         printf("Número de linhas invalido! Deve ser entre 1 e %d.\n", MAX_N);
         return 1;
     }
