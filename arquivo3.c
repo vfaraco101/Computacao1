2e224a099a43421a598fd69a4fc98de7722fbebc
vfaraco101 <vgfaraco@gmail.com>
2025-06-26 16:03:58 -0300

    Update arquivo3.c

--- arquivo3.c
+++ arquivo3.c
@@ -7,17 +7,20 @@ int main()
     int numero = 1;
 
     arquivo1 = fopen("teste1.txt", "r");
-    if (arquivo1 == NULL){
+    if (arquivo1 == NULL)
+    {
         printf("Erro ao abrir o arquivo\n");
         return 1;
     }
     arquivo2 = fopen("teste1_.txt", "w");
-    if (arquivo2 == NULL) {
+    if (arquivo2 == NULL)
+    {
         printf("Erro ao criar o arquivo\n");
         fclose(arquivo2);
         return 1;
     }
-    while (fgets(string, string, arquivo1) != NULL){
+    while (fgets(string, string, arquivo1) != NULL)
+    {
         fprintf(arquivo2, "%d. %s", numero++, string);
     }
     fclose(arquivo1);
