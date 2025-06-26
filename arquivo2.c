1551cbc7b00a932d9b5caf1ab7c4cd6b21b62465
vfaraco101 <vgfaraco@gmail.com>
2025-06-26 15:46:56 -0300

    Create arquivo2.c

---
+++ arquivo2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    FILE* arquivo;
+    float numero, soma;
+    char string [100];
+    arquivo = fopen("teste2.txt", "r");
+    if (arquivo == NULL) {
+        printf("Erro ao abrir o arquivo!\n");
+        return 1;
+    }
+    while (fscanf(arquivo, "%f", &numero) == 1) {
+        soma += numero;
+    }
+    fclose(arquivo);
+    printf("A soma dos valores e: %.2f\n", soma);
+    return (0);
+}
