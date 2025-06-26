89308a1c37db474c4a9635fde3c9de3bd409905c
vfaraco101 <vgfaraco@gmail.com>
2025-06-26 15:50:56 -0300

    Update arquivo2.c

--- arquivo2.c
+++ arquivo2.c
@@ -3,14 +3,16 @@
 int main()
 {
     FILE* arquivo;
-    float numero, soma;
+    float numero, soma = 0;
     char string [100];
     arquivo = fopen("teste2.txt", "r");
-    if (arquivo == NULL) {
+    if (arquivo == NULL)
+    {
         printf("Erro ao abrir o arquivo!\n");
         return 1;
     }
-    while (fscanf(arquivo, "%f", &numero) == 1) {
+    while (fscanf(arquivo, "%f", &numero) == 1)
+    {
         soma += numero;
     }
     fclose(arquivo);
